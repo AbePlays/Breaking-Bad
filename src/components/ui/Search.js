b8ee1ef7fda524c6e059b4ae1e6ebabcbdd0e693
Abe <abhi.rawat456@gmail.com>
2020-07-04 18:40:37 +0530

    Added state to the input

--- src/components/ui/Search.js
+++ src/components/ui/Search.js
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 
 const Search = () => {
+  const [text, setText] = useState("");
+
   return (
     <section className="search">
       <form>
@@ -9,6 +11,8 @@ const Search = () => {
           className="form-control"
           placeholder="Search Characters"
           autoFocus
+          value={text}
+          onChange={(e) => setText(e.target.value)}
         />
       </form>
     </section>
