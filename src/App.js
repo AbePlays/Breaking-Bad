49c86c83905e64a29e636f2821f38455152234cc
Abe <abhi.rawat456@gmail.com>
2020-07-04 18:32:17 +0530

    Created a character grid

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 import "./App.css";
 import Header from "./components/ui/Header";
+import CharacterGrid from "./components/characters/CharacterGrid";
 
 const App = () => {
   const [items, setItems] = useState([]);
@@ -24,6 +25,7 @@ const App = () => {
   return (
     <div className="container">
       <Header />
+      <CharacterGrid isLoading={isLoading} items={items} />
     </div>
   );
 };
